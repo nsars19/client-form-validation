df65ed6c7d6aa176ad7365be92e952189045931c
Nick <nsardonini@gmail.com>
2021-01-13 13:10:46 -0500

    prettify & lint main.js

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const form = document.querySelector("form");
-const [email, country, zip, pw, pwc, subBtn] = [...form.children];
+const [email, country, zip, pw, pwc] = [...form.children];
 const fields = [email, country, zip, pw, pwc];
 
 // prevent submitting if any are invalid
 form.addEventListener("submit", (e) => {
-  fields.forEach(field => {
+  fields.forEach((field) => {
     if (!field.checkValidity()) {
       field.className = "invalid invalid-wiggle";
       e.preventDefault();
     }
   });
 
-  
   if (pw.checkValidity() && pwc.checkValidity()) {
     if (pw.value !== pwc.value) {
-      [pw, pwc].forEach(field => field.className = "invalid invalid-wiggle");
+      [pw, pwc].forEach(
+        (field) => (field.className = "invalid invalid-wiggle")
+      );
     }
   }
 });
 
-fields.forEach(field => {
+fields.forEach((field) => {
   field.addEventListener("input", () => {
-    field.className = "";
-    field.checkValidity() ? field.className = "valid" : field.className = "invalid"
-  })
-})
+    field.checkValidity()
+      ? (field.className = "valid")
+      : (field.className = "invalid");
+  });
+});
