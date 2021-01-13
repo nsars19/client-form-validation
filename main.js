4b08d3f102dc158509874eb29224856ae1595413
Nick <nsardonini@gmail.com>
2021-01-13 12:59:38 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form");
+const [email, country, zip, pw, pwc, subBtn] = [...form.children];
+const fields = [email, country, zip, pw, pwc];
+
+// prevent submitting if any are invalid
+form.addEventListener("submit", (e) => {
+  fields.forEach(field => {
+    if (!field.checkValidity()) {
+      field.className = "invalid invalid-wiggle";
+      e.preventDefault();
+    }
+  });
+
+  
+  if (pw.checkValidity() && pwc.checkValidity()) {
+    if (pw.value !== pwc.value) {
+      [pw, pwc].forEach(field => field.className = "invalid invalid-wiggle");
+    }
+  }
+});
+
+fields.forEach(field => {
+  field.addEventListener("input", () => {
+    field.className = "";
+    field.checkValidity() ? field.className = "valid" : field.className = "invalid"
+  })
+})
